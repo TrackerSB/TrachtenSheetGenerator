84920b36667082a9f65c7497e59721a67b87063b
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-03-13 10:27:43 +0100

    Introduce Wizard dependency

--- src/main/java/de/traunviertler_traunwalchen/TrachtenSheetGenerator/Gui/MainMenuController.java
+++ src/main/java/de/traunviertler_traunwalchen/TrachtenSheetGenerator/Gui/MainMenuController.java
@@ -1,4 +1,28 @@
 package de.traunviertler_traunwalchen.TrachtenSheetGenerator.Gui;
 
+import bayern.steinbrecher.wizard.Wizard;
+import bayern.steinbrecher.wizard.WizardPage;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MainMenuController {
+
+    private static final Logger LOGGER = Logger.getLogger(MainMenuController.class.getName());
+
+    @FXML
+    private void startWizard() {
+        WizardPage<Void> finalPage = new WizardPage<Void>(new GridPane(), () -> null, true, () -> null);
+        Wizard wizard = new Wizard(Map.of(WizardPage.FIRST_PAGE_KEY, finalPage));
+        try {
+            wizard.start(new Stage());
+        } catch (IOException ex) {
+            LOGGER.log(Level.SEVERE, "Could not open main menu wizard", ex);
+        }
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module de.traunviertler_traunwalchen.TrachtenSheetGenerator {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.logging;
+    requires bayern.steinbrecher.Wizard;
 
     exports de.traunviertler_traunwalchen.TrachtenSheetGenerator.Gui;
+
+    opens de.traunviertler_traunwalchen.TrachtenSheetGenerator.Gui to javafx.fxml;
 }

63e9e772946bff30052485c713b6005eac119316
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-03-23 17:01:06 +0100

    Remove unused import

--- src/main/java/de/traunviertler_traunwalchen/TrachtenSheetGenerator/Gui/WizardPages/FreeFormPage.java
+++ src/main/java/de/traunviertler_traunwalchen/TrachtenSheetGenerator/Gui/WizardPages/FreeFormPage.java
@@ -3,7 +3,6 @@ package de.traunviertler_traunwalchen.TrachtenSheetGenerator.Gui.WizardPages;
 import bayern.steinbrecher.wizard.WizardableView;
 
 import java.util.Optional;
-import java.util.ResourceBundle;
 
 public class FreeFormPage extends WizardableView<Optional<String>, FreeFormPageController> {
 

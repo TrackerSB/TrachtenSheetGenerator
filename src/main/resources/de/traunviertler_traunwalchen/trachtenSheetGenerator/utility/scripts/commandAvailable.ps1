ea07aad00b9985d485f4a5eea1a6028b80eacec9
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-04-22 16:20:04 +0200

    Mark parameters as mandatory
    Signed-off-by: Stefan Huber <stefan.huber.niedling@outlook.com>

--- src/main/resources/de/traunviertler_traunwalchen/trachtenSheetGenerator/utility/scripts/commandAvailable.ps1
+++ src/main/resources/de/traunviertler_traunwalchen/trachtenSheetGenerator/utility/scripts/commandAvailable.ps1
@@ -1,5 +1,5 @@
 # Checks whether the given command $cmdName can be found and executed
-param([string] $cmdName)
+param([Parameter(Mandatory=$true)][string] $cmdName)
 
 if (Get-Command $cmdName -errorAction SilentlyContinue) {
     Write-Output "yes"
